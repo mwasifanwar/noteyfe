0cef4f69c35478f8930be0487553de954c56a67b
Muhammad Wasif <76221199+mwasifanwar@users.noreply.github.com>
2025-01-26 17:14:23 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    headers: {
+      'Content-Security-Policy':
+        "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; connect-src 'self' https://noteybe.onrender.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:;",
+    },
   },
 });
